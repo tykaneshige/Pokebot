4fa6e8fb8a60bfbdc39b4e20f69f5ec12fb48c2f
Nicholas Yamamoto <nicholas.yamamoto@gmail.com>
2020-07-30 16:46:52 -0700

    ping pong

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import discord
 
 client = discord.Client()
-
+prefix = '!'
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -12,7 +12,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith(prefix + 'test'):
         counter = 0
         tmp = await message.channel.send('Calculating messages...')
         '''
@@ -26,5 +26,10 @@ async def on_message(message):
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await message.channel.send('Done sleeping')
+    elif message.content.startswith(prefix + 'ping'):
+        tmp = await message.channel.send('pong!')
 
-client.run('Insert Token Here')
+
+
+
+client.run('NzM4MTk4NzkwNjk5MzUyMTE1.XyIbMg._srdWL9_GtNqTHG6yTwtRUabDVs')
