4eb04abe04e2a6c4488c56106b130b9d032ea4de
tykaneshige <tykaneshige.work@gmail.com>
2020-07-30 17:18:53 -0700

    Basic implementation using the bot subclass.

--- bot.py
+++ bot.py
@@ -1,35 +1,24 @@
 import asyncio
 import discord
+from discord.ext import commands
 
 client = discord.Client()
-prefix = '!'
-@client.event
+bot = commands.Bot(command_prefix='!')
+
+# Bot ommands
+@bot.command
+async def ping(ctx):
+    await ctx.send('pong!')
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.content.startswith(prefix + 'test'):
-        counter = 0
-        tmp = await message.channel.send('Calculating messages...')
-        '''
-        async for log in guild.audit_logs(limit=100):
-            if log.author == message.author:
-                counter += 1
+    await bot.process_commands(message)
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-        '''
-        await message.channel.send('I can\'t count')
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await message.channel.send('Done sleeping')
-    elif message.content.startswith(prefix + 'ping'):
-        tmp = await message.channel.send('pong!')
-
-
-
-
-client.run('NzM4MTk4NzkwNjk5MzUyMTE1.XyIbMg._srdWL9_GtNqTHG6yTwtRUabDVs')
+bot.run('Insert Token Here')
