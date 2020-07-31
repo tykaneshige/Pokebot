df6b4851209824c231c86f184bff8ad3287c4038
tykaneshige <tykaneshige.work@gmail.com>
2020-07-30 17:26:39 -0700

    Implemented an exit command.

--- bot.py
+++ bot.py
@@ -6,9 +6,14 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='!poke ')
 
 # Bot ommands
-@bot.command
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong!')
+
+@bot.command()
+async def exit(ctx):
+    await ctx.send('Shutting down.')
+    await bot.close()
 
 @bot.event
 async def on_ready():
@@ -21,4 +26,4 @@ async def on_ready():
 async def on_message(message):
     await bot.process_commands(message)
 
-bot.run('Insert Token Here')
+bot.run('NzM4MTk4NzkwNjk5MzUyMTE1.XyIbMg.TCublKX6CjNp3U2R9PZUZobY8ow')
