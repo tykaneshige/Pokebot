8f6c8070fc61db7a9bf2833c7168b57f3d4d0767
Nicholas Yamamoto <nicholas.yamamoto@gmail.com>
2020-08-13 15:27:07 -0700

    Baseline Pokebot with message counter

---
+++ Pokebot.py
@@ -0,0 +1,39 @@
+import asyncio
+import discord
+
+from discord.ext import commands
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='!poke ')
+Token = 'NzQzNTcwNDQ2OTg0ODA2NDcw.XzWl8Q.gEWDg-RI8UPdGNpLbfcvfjTEJ_8'
+
+
+
+class Pokebot(commands.Cog) :
+    
+    def __init__(self,bot):
+        self.bot=bot
+        self.counter = 0
+    
+    # Bot Commands
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send('pong!')
+    
+    @commands.command()
+    async def exit(self,ctx):
+        await ctx.send('Shutting down.')
+        await self.bot.close()
+
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        if message.author.id is not self.bot.user.id and not message.content.startswith("!poke "):
+            self.counter+=1
+
+    @commands.command()
+    async def count(self,ctx):
+        await ctx.send(self.counter)
+
+if __name__ == '__main__' :
+    bot.add_cog(Pokebot(bot))
+    bot.run(Token)
